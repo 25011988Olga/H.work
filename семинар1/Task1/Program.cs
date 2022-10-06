9fbfa9aeafe593191b2aac63777a5d0604d0e40e
25011988Olga <olga.moseychuk.88@mail.ru>
2022-10-06 12:12:25 +0300

    выполнилфTask1

---
+++ семинар1/Task1/Program.cs
@@ -0,0 +1,16 @@
+﻿Console.Write("введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+if(a>b)
+{
+    Console.WriteLine("первое число max,второе min");
+}
+if(a<b)
+{
+    Console.WriteLine("первое число min,второе max");
+}
+if(a==b)
+{
+    Console.WriteLine("числа равны,введите новую пару чисел");
+}
