b661b9f9ec9330519b7dc14bd21c9b14856dbd9c
25011988Olga <olga.moseychuk.88@mail.ru>
2022-10-06 12:36:20 +0300

    ВыполнилА Task2

---
+++ семинар1/Task2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число; ");
+int b = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введи третье число: ");
+int c = Convert.ToInt32(Console.ReadLine());
+int max = a;
+if (b>max) max = b;
+{
+    Console.WriteLine("Второе число max");
+}
+if(c>max) max = c;
+{
+    Console.WriteLine("Третье число max");
+}
