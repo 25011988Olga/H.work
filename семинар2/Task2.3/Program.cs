14fe6326d2e9de909d56812029bea754bb8dbf6b
25011988Olga <olga.moseychuk.88@mail.ru>
2022-10-08 00:31:36 +0300

    выполнила Task2.3

---
+++ семинар2/Task2.3/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.Write("введите число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+while(n<1||n>7)
+n = Convert.ToInt32(Console.ReadLine());
+if(n>5)
+Console.WriteLine("выходной");
+if (n<6)
+Console.WriteLine("будний день"); 
